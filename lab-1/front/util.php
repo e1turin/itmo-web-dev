509377f8551083b0c9ae70a714c42cdce0a36713
e1turin <pass>
2022-08-24 14:42:58 +0300

    completed desine of website

---
+++ lab-1/front/util.php
@@ -0,0 +1,8 @@
+<?php
+
+function selection_bubble($type, $id){
+    return "<div class=\"sel-bubble\">
+                <label for=\"{$id}\">-6</label >
+                <input type=\"{$type}\" id=\"{$id}\" />  
+            </div>";
+}
---
+++ lab-1/index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <link rel="stylesheet" href="front/style.css"/>
+    <title>ЛР №1</title>
+</head>
+<body>
+<header>Тюрин Иван 335047</header>
+
+<div class="main">
+    <div class="areas">
+        <img src="../areas.png" alt="areas"/>
+        <!-- <canvas></canvas> -->
+    </div>
+    <div class="table">
+        <span>Предыдущие попытки</span>
+        <hr/>
+        <table>
+            <thead>
+            <tr>
+                <th>X</th>
+                <th>Y</th>
+                <th>R</th>
+                <th>AC</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>miss</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>miss</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>miss</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>miss</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+    <form class="coords" action="check.php" method="get">
+        <div class="x-coord">
+            <span>X:</span>
+            <!-- div.checkboxes>div.sel-bubble*9>label[for=x-chkbox-$]+input[type=checkbox id=x-chkbox-$] -->
+            <div class="x-chkboxes">
+                <?php
+                $x_values = array(-5, -4, -3, -2, -1, 0, 1, 2, 3);
+                for ($i = 0; $i < count($x_values); $i++) {
+                    echo "<div class=\"sel-bubble\">
+                            <label for=\"x-chkbox-{$i}\">{$x_values[$i]}</label
+                            ><input type=\"checkbox\" id=\"x-chkbox-{$i}\" />
+                          </div>";
+                }
+                ?>
+            </div>
+        </div>
+
+        <div class="y-coord">
+            <span>Y:</span>
+            <label for="">
+                <input type="text" name="" id=""/>
+            </label>
+            <span>field must contain number</span>
+        </div>
+
+        <div class="r-coord">
+            <span>R:</span>
+            <div class="r-radios">
+                <?php
+                $y_values = array(1, 2, 3, 4, 5);
+                for ($i = 0; $i < count($y_values); $i++) {
+                    echo "<div class=\"sel-bubble\">
+                            <label for=\"r-radio-{$i}\">{$y_values[$i]}</label
+                            ><input type=\"radio\" id=\"r-radio-{$i}\" name=\"r-coord-radio\"/>
+                          </div>";
+                }
+                ?>
+            </div>
+        </div>
+        <div class="coords-submit-btn">
+            <input class="submit-btn" type="submit" value="Save">
+        </div>
+    </form>
+</div>
+
+<footer>footer</footer>
+</body>
+</html>
