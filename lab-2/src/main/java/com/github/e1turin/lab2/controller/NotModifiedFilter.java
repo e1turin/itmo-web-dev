11cfc7307eb5fa741e9169f4666c4e1e052ff4f7
e1turin <pass>
2022-11-03 18:03:20 +0300

    Extra task for 'If-Modified-Since' header interop (setup filter and some refactoring)

--- lab-2/src/main/java/com/github/e1turin/lab2/controller/NotModifiedFilter.java
+++ lab-2/src/main/java/com/github/e1turin/lab2/controller/NotModifiedFilter.java
@@ -4,12 +4,15 @@ import jakarta.servlet.*;
 import jakarta.servlet.annotation.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
 @WebFilter(filterName = "NotModifiedFilter")
 public class NotModifiedFilter implements Filter {
     private FilterConfig config;
+    private static final Logger logger = LoggerFactory.getLogger(NotModifiedFilter.class);
 
     public void init(FilterConfig config) throws ServletException {
         Filter.super.init(config);
@@ -23,9 +26,12 @@ public class NotModifiedFilter implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws ServletException, IOException {
         HttpServletRequest req = (HttpServletRequest) servletRequest;
         HttpServletResponse resp = (HttpServletResponse) servletResponse;
+        logger.info("URL: " + req.getRequestURL());
+
         long lastModifiedFromBrowser = req.getDateHeader("If-Modified-Since");
         long lastModifiedFromServlet = getLastModifiedFromServlet();
         if (lastModifiedFromBrowser != -1 && lastModifiedFromServlet != -1 && lastModifiedFromServlet < lastModifiedFromBrowser) {
+            logger.info("Filtered URL: " + req.getRequestURL() + ", session id:" + req.getSession().getId());
             resp.setStatus(HttpServletResponse.SC_NOT_MODIFIED);
             return;
         }
