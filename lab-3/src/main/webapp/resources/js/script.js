a29ed7713fa724921259975cd3f02038f7c3edbd
e1turin <pass>
2023-01-03 23:48:38 +0300

    Docs for js script.js

--- lab-3/src/main/webapp/resources/js/script.js
+++ lab-3/src/main/webapp/resources/js/script.js
@@ -6,10 +6,10 @@ window.onload = () => {
     draw_scene()
 }
 
-const point_r = 5 //px
-const line_width = 4 //px
-const border_offset = 10 //px
-const possible_R = [1, 2, 3, 4, 5] //m
+const point_r = 5 // [px]
+const line_width = 4 // [px]
+const border_offset = 10 // [px]
+const possible_R = [1, 2, 3, 4, 5] // [dp] = [Density-independent Pixels]
 const point_colors = {in: '#09911b', out: '#c71717'}
 const target_colors = {axis: '#2c2c2c', background: '#eec90f', area: '#2c71f5'}
 
@@ -31,9 +31,10 @@ const on_r_change = (event) => {
 
 
 let cft = () => {
-    return Math.min(cnv.width, cnv.height) / Math.max(...possible_R) // px/m
+    return Math.min(cnv.width, cnv.height) / Math.max(...possible_R) // [px/dp]
 }
 
+/* returns center point of canvas [px] */
 let mid = () => {
     return {x: cnv.width / 2, y: cnv.height / 2}
 }
@@ -43,12 +44,14 @@ const get_points = () => {
     return points
 }
 
+/* draws circle with center in point (`x`, `y`) with radius `r`, all are [px] */
 const draw_circ = ({x, y}, r) => {
     ctx.beginPath();
     ctx.arc(x, y, r, 0, 7);
     ctx.fill();
 }
 
+/* draws point with center in point (`x`, `y`) [dp] with fill color corresponding position inside or outside area */
 const draw_point = ({x, y, inside}) => {
     ctx.fillStyle = inside === true ? point_colors.in : point_colors.out
     draw_circ({
@@ -57,10 +60,12 @@ const draw_point = ({x, y, inside}) => {
     }, point_r)
 }
 
+/* draws rectangle from top left point (`x1`, `y1`) [px] to bottom right point (`x2`, `y2`) [px] */
 const draw_rect = ({x1, y1}, {x2, y2}) => {
     ctx.fillRect(x1, y1, x2 - x1, y2 - y1);
 }
 
+/* draws triangle with 3 points: (`x1`, `y1`), (`x2`, `y2`), (`x3`, `y3`), all in [px] */
 const draw_triangle = ({x1, y1}, {x2, y2}, {x3, y3}) => {
     ctx.beginPath();
     ctx.moveTo(x1, y1);
@@ -69,20 +74,22 @@ const draw_triangle = ({x1, y1}, {x2, y2}, {x3, y3}) => {
     ctx.fill();
 }
 
+/* draws axes of the target on the canvas with specified `line_width` */
 const draw_axes = () => {
-    /* horizontal */
+    /* horizontal axis X */
     draw_rect(
-        {x1: /*  0 +  */ border_offset, y1: mid().y + line_width / 2},
+        {x1: /*__0_+__*/ border_offset, y1: mid().y + line_width / 2},
         {x2: cnv.width - border_offset, y2: mid().y - line_width / 2}
     )
 
-    /* vertical */
+    /* vertical axis Y */
     draw_rect(
-        {x1: mid().x - line_width / 2, y1: border_offset /*  + 0   */},
+        {x1: mid().x - line_width / 2, y1: border_offset /*__+_0___*/},
         {x2: mid().x + line_width / 2, y2: cnv.height - border_offset}
     )
 }
 
+/* draws bottom right sector of round with center in point (`x`, `y`) [px] and radius `r` [px] */
 function draw_sector({x, y}, r) {
     ctx.beginPath();
     ctx.arc(x, y, r, 0, Math.PI / 2);
@@ -96,14 +103,14 @@ function draw_sector({x, y}, r) {
 
 const draw_areas = () => {
     draw_triangle(
-        {x1: mid().x, /*            */  y1: mid().y},
-        {x2: mid().x, /*            */  y2: mid().y - R() * cft()},
+        {x1: mid().x, /*____________*/  y1: mid().y},
+        {x2: mid().x, /*____________*/  y2: mid().y - R() * cft()},
         {x3: mid().x + R() / 2 * cft(), y3: mid().y}
     )
 
     draw_rect(
         {x1: mid().x - R() * cft(), y1: mid().y},
-        {x2: mid().x, /*         */ y2: mid().y + R() * cft()}
+        {x2: mid().x, /*_________*/ y2: mid().y + R() * cft()}
     )
 
     draw_sector(mid(), R() / 2 * cft())
@@ -136,6 +143,7 @@ const on_update = () => {
     draw_scene()
 }
 
+/* return point (`x`, `y`) [px] of canvas */
 function get_mouse_position(canvas, event) {
     let rect = canvas.getBoundingClientRect();
     return {
